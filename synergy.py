7124e4cde3a3de2fe6d6312c08017a593e164904
Валерия Рутковская <217555@edu.fa.ru>
2026-08-24 22:55:09 +0300

    Выполненные задания урока 3

--- synergy.py
+++ synergy.py
@@ -1,2 +1,29 @@
+# Урок 2
 # тут я 
-print(7)
+# print(7)
+# Урок 3
+# a = 3 + 5
+# b = 3 - 5
+# c = 5 / 2
+# d = 5 // 2
+# e = 3 * 4
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# a = 5
+# a += 10
+# print(a)
+# a = int(input())
+# b = int(input())
+# print(a * b)
+# a, b, c = map(int, input().split())
+# print(a * 2)
+# a, b = map(int, input().split())
+# c = a + b
+# print(c)
+# a = 10
+# b = 3
+# print(a / b * 3)
+# print(float(input()))
