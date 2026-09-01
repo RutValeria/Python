bfdfb74a48cf3ac1c0ee5f8b01434e1647cbfdbd
Валерия Рутковская <217555@edu.fa.ru>
2026-09-01 21:24:13 +0300

    Урок 5

--- 5Zadanie3.py
+++ 5Zadanie3.py
@@ -4,6 +4,16 @@
 # больше инвестировать можно сколько угодно. У Майкла A долларов,
 # у Ивана B долларов. Если оба могут вложиться - выведите 2, если только Майкл - Mike,
 # если только Иван - Ivan, если не могут по отдельности, но вместе им хватает - 1, если никто - 0.
-Invest = int(input())
+SumInvest = int(input())
 Mike = int(input())
 Ivan = int(input())
+if (Mike >= SumInvest) and (Ivan >= SumInvest):
+    print("2")
+elif (Mike >= SumInvest) and (Ivan < SumInvest):
+    print("Mike")
+elif (Ivan >= SumInvest) and (Mike < SumInvest):
+    print("Ivan")
+elif ((Mike < SumInvest) or (Ivan < SumInvest)) and (Mike + Ivan >= SumInvest):
+    print("1")
+else:
+    print("0")
